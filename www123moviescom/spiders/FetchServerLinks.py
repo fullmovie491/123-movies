6e01ecc2cb8b73eaf57caa16052558043906c374
PRANEETH <PRANEETH@PRANEETHs-MacBook-Pro.local>
2017-12-01 10:42:32 +0530

    changes

--- www123moviescom/spiders/FetchServerLinks.py
+++ www123moviescom/spiders/FetchServerLinks.py
@@ -5,23 +5,25 @@ import re
 
 class FetchServerLinksSpider(CrawlSpider):
     name = 'FetchServerLinks'
-    
-    allowed_domains = ['123movies.unblockall.org'] # Which (sub-)domains shall be scraped?
-    start_urls = ['http://123movies.unblockall.org/watch/QG3oA8Go-hack-sign-sub.html'] # Start with this one
+    global embed_list
+    embed_list = []
+    allowed_domains = ['http://123moviesonline.stream']#123movies.unblockall.org'] # Which (sub-)domains shall be scraped?
+    start_urls = ['http://123moviesonline.stream/watch/QG3oA8Go-hack-sign-sub.html'] # Start with this one
     rules = [Rule(LinkExtractor(allow=(r'\/watch\/'),deny=(r'(season|\/watch\/.+\/|episode)')), callback='watch_page',follow=True)]# Follow any link scrapy finds (that is allowed).
 
     def watch_page(self,response):
         main_url = response.url
-        print main_url
     	if not (re.search(r'(season|episode)',response.css('title::text').extract_first(),re.IGNORECASE)):
     		if not (response.css('#details.section-box')):
 		        for server in response.css('.server_play a::attr(href)').extract():
 		            yield response.follow(server,self.get_server_links,meta={'main_url':main_url})
+                print embed_list
             
                
 
     def get_server_links(self,response):
 
+        embed_list.append(response.css('div#media-player  script::text').extract_first())
         
     	yield{
                 'main_url' : response.meta.get('main_url'),
